aaa28e3204b73d9984e2b1813d619f92fe2f1de0
sam-mhondiwa81 <smhonds@gmail.com>
2022-05-10 19:04:45 +0200

    Adding products and creating a shopping cart

--- crown-clothing/src/routes/navigation/navigation.component.jsx
+++ crown-clothing/src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,8 @@
 import { Fragment, useContext }from 'react';
 import { Outlet, Link } from 'react-router-dom';
+
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+
 import {ReactComponent as CrownLogo } from '../../assets/crown.svg';
 import './navigation.styles.scss';
 import {UserContext} from '../../contexts/user.context';
@@ -31,7 +34,7 @@ const Navigation = () => {
                             SIGN IN
                          </Link>
                         )}
-                    
+                      <CartIcon />
                 </div>
             </div>
             <Outlet />
