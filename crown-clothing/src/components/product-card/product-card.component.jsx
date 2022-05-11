e5e1cff3c647095ad8c51cd4e5d82d7cca0a629d
sam-mhondiwa81 <smhonds@gmail.com>
2022-05-11 21:31:48 +0200

    Added my addToCart feature

--- crown-clothing/src/components/product-card/product-card.component.jsx
+++ crown-clothing/src/components/product-card/product-card.component.jsx
@@ -1,10 +1,18 @@
-import './product-card.styles.scss';
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button.component';
 
-const ProductCard = ({product}) => {
 
+import './product-card.styles.scss';
+
+const ProductCard = ({product}) => {
+     
     const {name, price, imageUrl} = product;
+    const { addItemToCart } = useContext(CartContext);
+
+    const addProductToCart = () => addItemToCart(product);
 
     return(
         <div className="product-card-container">
@@ -13,7 +21,7 @@ const ProductCard = ({product}) => {
                <span className="name">{name}</span>
                <span className="price">{price}</span>
            </div>
-           <Button buttonType='inverted'>Add to Cart</Button>
+           <Button buttonType='inverted' onClick={addProductToCart}>Add to Cart</Button>
         </div>
     );
 }
--- crown-clothing/src/routes/navigation/navigation.component.jsx
+++ crown-clothing/src/routes/navigation/navigation.component.jsx
@@ -2,15 +2,17 @@ import { Fragment, useContext }from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
-
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import {ReactComponent as CrownLogo } from '../../assets/crown.svg';
 import './navigation.styles.scss';
 import {UserContext} from '../../contexts/user.context';
+import {CartContext} from '../../contexts/cart.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 const Navigation = () => { 
 
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
     // console.log(currentUser);
 
     return(
@@ -36,6 +38,7 @@ const Navigation = () => {
                         )}
                       <CartIcon />
                 </div>
+                { isCartOpen && <CartDropdown />}
             </div>
             <Outlet />
         </Fragment>
