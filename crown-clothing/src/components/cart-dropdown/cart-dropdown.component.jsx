16641862f0c99c5414bc591e89595683eed83f09
sam-mhondiwa81 <smhonds@gmail.com>
2022-05-14 02:08:51 +0200

    Created checkout page, checkout items along with checkout cart functionalities

--- crown-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crown-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,5 @@
 import { useContext} from 'react';
-
+import {useNavigate} from 'react-router-dom';
 import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button.component';
@@ -10,6 +10,11 @@ import './cart-dropdown.styles.scss';
 const CartDropDown = () => {
 
     const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goToCheckOutHandler = () => {
+        navigate('/checkout');
+    }
 
     return(
         <div className="cart-dropdown-container">
@@ -18,7 +23,7 @@ const CartDropDown = () => {
                     <CartItem key={item.id} cartItem={item}/>
                 ))}
              </div>
-             <Button>GO TO CHECKOUT</Button>
+             <Button buttonType="google-sign-in" onClick={goToCheckOutHandler} >CHECKOUT</Button>
         </div>
     );
 } 
